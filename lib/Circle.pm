6adf898e931fd9aab5e0fdd50b334330b1dde176
Hiroki Suenaga <hsuehiro5@gmail.com>
2011-12-18 22:41:29 +0900

    円クラスのモジュール

---
+++ lib/Circle.pm
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+# lib/Circle.pm
+
+use strict;
+use warnings;
+
+package Circle;
+
+sub new{
+    my $class = shift;
+    my $self = { r => shift };
+    bless $self,$class;
+}
+
+sub hankei {
+  return shift->{r};
+}
+
+sub menseki{
+  my  $pai = 3.14;
+  return shift->hankei()**2*$pai;
+}
+
+1;
