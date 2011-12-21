94e3537dbafc62641f1bb1dda6822f640472d5dc
Hiroki Suenaga <hsuehiro5@gmail.com>
2011-12-22 03:27:07 +0900

    APIで関東のJRがつく路線だけ表示させてみる

--- train.pl
+++ train.pl
@@ -6,13 +6,13 @@ use Data::Dumper;
 my $api = 'http://express.heartrails.com/api/xml?method=getLines&area=';
 my $area = '関東';
 
-my $document = LWP::Simple::get($api.$area) or die 'cannot get content fromj $url';
+my $document = LWP::Simple::get($api.$area) or die 'cannot get content fromj $area';
 
 my $parser = XML::Simple->new;
 my $data = $parser->XMLin($document);
 
 foreach my $data (@{$data->{line}}){
-    print "$data\n";
+    print "$data\n" if $data =~s/^JR/ig;
 }
 
     
