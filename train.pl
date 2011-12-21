5b9ba1947a93b92bbde8f275df1a00a083a8c781
Hiroki Suenaga <hsuehiro5@gmail.com>
2011-12-22 03:05:43 +0900

    APIを使い関東の路線全部表示する

---
+++ train.pl
@@ -0,0 +1,18 @@
+use strict;
+use LWP::Simple;
+use XML::Simple;
+use Data::Dumper;
+
+my $api = 'http://express.heartrails.com/api/xml?method=getLines&area=';
+my $area = '関東';
+
+my $document = LWP::Simple::get($api.$area) or die 'cannot get content fromj $url';
+
+my $parser = XML::Simple->new;
+my $data = $parser->XMLin($document);
+
+foreach my $data (@{$data->{line}}){
+    print "$data\n";
+}
+
+    
