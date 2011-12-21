4422e7ada25f0556ca8cb1037e939ecb901eca65
Hiroki Suenaga <hsuehiro5@gmail.com>
2011-12-22 05:21:29 +0900

    ある地域における２つの都道府県の路線および、共通の路線

--- train.pl
+++ train.pl
@@ -1,18 +1,47 @@
 use strict;
+use warnings;
 use LWP::Simple;
 use XML::Simple;
 use Data::Dumper;
+use utf8;
 
-my $api = 'http://express.heartrails.com/api/xml?method=getLines&area=';
-my $area = '関東';
+my $api = 'http://express.heartrails.com/api/xml?method=getLines';
+my $area = '&area=関東';
+my $prefecture  = '&prefecture=埼玉県';
+my $pre2 = '&prefecture=東京都';
 
-my $document = LWP::Simple::get($api.$area) or die 'cannot get content fromj $area';
+my $document = LWP::Simple::get($api.$area.$prefecture) or die 'cannot get content fromj $area';
+
+my $document2 = LWP::Simple::get($api.$area.$pre2) or die 'cannot get content from $pre2';
 
 my $parser = XML::Simple->new;
 my $data = $parser->XMLin($document);
+my $data2 = $parser->XMLin($document2);
 
-foreach my $data (@{$data->{line}}){
-    print "$data\n" if $data =~s/^JR/ig;
+my @train1;
+my @train2;
+
+foreach my $data(@{$data->{line}}){
+    push (@train1,$data); 
 }
 
-    
+print join(',',@train1);
+
+print "$pre2\n\n";
+foreach my $data2(@{$data2->{line}}){
+    push (@train2,$data2);
+}
+
+print join(',',@train2);
+
+my @match = ();
+
+foreach my $str(@{$data->{line}}){
+    grep($str eq $_,@{$data2->{line}}) && push(@match,$str);
+}
+
+print "\n\n共通路線\n\n";
+
+print join(',',@match);
+
+print "\n";
